d98d1c4d334089a6a338d9f5fae6f176f5594ca2
Cirdans-Home <f.durastante@na.iac.cnr.it>
2021-04-30 15:43:54 +0200

    Set static site

---
+++ assets/js/database.js
@@ -0,0 +1,83 @@
+window.database = {
+  
+    
+      
+    
+      
+    
+      
+    
+    "": {
+      "title": "Home",
+      "category": "",
+      "content": "### INdAM workshop on Fractional Differential Equations:Modeling, Discretization, and Numerical Solvers      July 12-14, 2021        Rome & Online  The INdAM workshop \"Fractional Differential Equations: Modeling, Discretization,and Numerical Solvers\" will take place in Rome, July 12-14, 2021 and *partially*online.The **in presence** part of the workshop will take place at Best WesternGlobus Hotel in Viale Ippocrate, 119, 00161, Roma (RM), inside the Ping room.#### The workshopFractional calculus deals with the study and application of integrals andderivatives of non-integer order. These operators, unlike the classic operatorsof integer order,  are non-local operators and are better suited to describephenomena with memory (with respect to time and/or space).Although the basic ideas of fractional calculus go back over three centuries,only in recent decades has there been a rapid increase in interest towardsthis field of research, due not only to the increasing use fractional calculusin applications in *biology*, *physics*, *engineering*, *probability*, etc., butalso thanks to the availability of new and more powerful **numerical tools** thatallows for an **efficient solution** of problems that until a few years ago appearedunsolvable.In fact, numerical analysis plays a decisive role in fractional calculus.The analytical solution of fractional differential equations (FDEs) appears evenmore difficult than in the integer case and, therefore, practically every type ofapplication of fractional calculus requires adequate numerical tools.This workshop, therefore, aims to bring together the two communities of numericalanalysts operating in this field -- the ones working on methods for the solution ofdifferential problems and the one working on the numerical linear algebra side --to *share knowledge* and *create synergies*.It also intends to encourage meetings with researchers who deal with these problems froma more applicative point of view, both in fields such as mathematical physics and infields with an higher technological content such as image recognition and engineering(especially in the automatic control of systems).The **main topics** of the workshop are:- Numerical methods for differential problems of fractional order- Solving systems of large linear equations with dense matrices and use of preconditioners- Fractional Laplacian- Image processing using fractional operators- Application in physics of fractional operators- Machine learning techniques in the fractional environment",
+      "url": "/",
+      "href": "/"
+    }
+    ,
+  
+    
+    "organizers-html": {
+      "title": "Organizers",
+      "category": "",
+      "content": "## Organizing committee- [Angelamaria Cardone](https://docenti.unisa.it/005020/home), Università di Salerno- [Marco Donatelli](http://scienze-como.uninsubria.it/mdonatelli/), Università dell'Insubria- [Fabio Durastante](https://fdurastante.github.io/), Università di Pisa- [Roberto Garrappa](https://www.dm.uniba.it/members/garrappa/main), Università degli Studi di Bari- [Mariarosa Mazza](https://sites.google.com/site/mariarosamazza1/), Università dell'Insubria- [Marina Popolizio](http://deipoliba.azurewebsites.net/marina-popolizio/), Politecnico di Bari### FundingThe workshop is financed by- ![INDAM](/img/indamlogo.png){: style=\"height:40px; vertical-align: middle;\"} -- **I**stituto **N**azionale di **A**lta **M**atematica \"Francesco Severi\"{{site.data.alerts.note}}For information write to fractional@gmx.com.{{site.data.alerts.end}}",
+      "url": "/organizers.html",
+      "href": "/organizers.html"
+    }
+    ,
+  
+    
+    "participants-html": {
+      "title": "Participants",
+      "category": "",
+      "content": "## Participants Organizer  Participant-  Jin Bangti, University College London-  Angelamaria Cardone, Università di Salerno-  Marta D'Elia, Sandia National Laboratories-  Marco Donatelli, Università dell'Insubria-  Fabio Durastante, Università di Pisa-  Roberto Garrappa, Università degli Studi di Bari-  Luca Gerardo Giorda, Johannes Kepler Universität-  Andrea Giusti, ETH Zurich-  Mariarosa Mazza, Università dell'Insubria-  Paolo Novati, Università di Trieste-  Michael Kwok-Po Ng, The University of Hong Kong-  John Pearson, The University of Edinburgh-  Francesca Sabrina Pellegrino, Libera Università Mediterranea-  Laura Pezza, Università di Roma \"La Sapienza\"-  Francesca Pitolli, Università di Roma \"La Sapienza\"-  Marina Popolizio, Politecnico di Bari-  Abner Salgado, University of Tennessee-  Igor Simunec, Scuola Normale Superiore-  Ken Lorenzo Trotti, Università dell'Insubria-  Alessandra Jannelli, Università di Messina- ...{% include note.html content=\"In the process of completion.\" %}{% leaflet_map {\"center\" : [42.061443, 1.5682628], \"zoom\" : 1.8} %}        {% leaflet_marker {\"latitude\": 41.108029,        \"longitude\": 16.880532,        \"popupContent\" : \"Politecnico di Bari\"} %}        {% leaflet_marker {\"latitude\": 43.7198241,        \"longitude\": 10.4075959279568,        \"popupContent\" : \"Università di Pisa\"} %}        {% leaflet_marker {\"latitude\": 43.71966865,        \"longitude\": 10.4004284558292,        \"popupContent\" : \"Scuola Normale Superiore\"} %}        {% leaflet_marker {\"latitude\": 40.7739442,        \"longitude\": 14.7938122,        \"popupContent\" : \"Università di Salerno\"} %}        {% leaflet_marker {\"latitude\": 45.801175,        \"longitude\": 9.095095,        \"popupContent\" : \"Università dell'Insubria\"} %}          {% leaflet_marker {\"latitude\": 37.6798843365303,        \"longitude\": -121.70625607301,        \"popupContent\" : \"Sandia National Laboratories\"} %}        {% leaflet_marker {\"latitude\": 48.3374934,        \"longitude\": 14.3227876904811,        \"popupContent\" : \"Johannes Kepler Universität \"} %}        {% leaflet_marker {\"latitude\": 47.3764545,        \"longitude\": 8.5481666,        \"popupContent\" : \"ETH Zurich\"} %}            {% leaflet_marker {\"latitude\": 45.657638,        \"longitude\": 13.800986,        \"popupContent\" : \"Università di Trieste\"} %}              {% leaflet_marker {\"latitude\": 55.921671,        \"longitude\": -3.175056,        \"popupContent\" : \"University of Edinburgh\"} %}          {% leaflet_marker {\"latitude\": 22.2613573,        \"longitude\": 114.1368089,        \"popupContent\" : \"The University of Hong Kong\"} %}        {% leaflet_marker {\"latitude\": 35.9516352,        \"longitude\": -83.930881879626,        \"popupContent\" : \"University of Tennessee\"} %}            {% leaflet_marker {\"latitude\": 41.901840,        \"longitude\": 12.511840,        \"popupContent\" : \"Università di Roma &#34;La Sapienza&#34;\"} %}             {% leaflet_marker {\"latitude\": 40.961270,        \"longitude\": 16.928260,        \"popupContent\" : \"Libera Università Mediterranea\"} %}           {% leaflet_marker {\"latitude\": 38.260960,        \"longitude\": 15.600130,        \"popupContent\" : \"Università di Messina\"} %}             {% endleaflet_map %}",
+      "url": "/participants.html",
+      "href": "/participants.html"
+    }
+    ,
+  
+    
+    "program-html": {
+      "title": "Program",
+      "category": "",
+      "content": "## Program and abstractsThe workshop will have the following program. It will be possible to follow allthe talks online through the TBD platform.|              | Monday, 12      | Tuesday, 13    | Wednesday, 14 ||--------------|---------------  |----------------|---------------||              | [9.30 - 17.30]  | [9.30 - 17.30] | [9.30 - 16.00]|  {:.focustablestyle}{% include tip.html content=\"Lunches will be covered by the organization.\" %}### How to follow the workshop on-lineThe talks can be followed by connecting to the TBD platform through the link.### Keynote speakersThe keynote lectures last 50 minutes. Their purpose is to establish a mainunderlying theme with respect to the Workshop's aims.               Speaker      Title              {% for author in site.data.keynotes %}          {{ author.time }}       {{ author.author }}       {{ author.title }}                {% endfor %}{% for author in site.data.keynotes %}#### {{ author.title }}{{ author.abstract | markdownify }}{% endfor %}### Talks              Speaker      Title              {% for author in site.data.talks %}          {{ author.time }}       {{ author.author }}       {{ author.title }}                {% endfor %}{% for author in site.data.talks %}#### {{ author.title }}{{ author.abstract | markdownify }}{% endfor %}### BookletAll relevant information and abstracts are contained in the abstract bookwhich is [ available here in .pdf format.](./file/bookofabstract.pdf){{site.data.alerts.note}}For information write to fractional@gmx.com.{{site.data.alerts.end}}",
+      "url": "/program.html",
+      "href": "/program.html"
+    }
+    ,
+  
+    
+    "registration-html": {
+      "title": "Registration",
+      "category": "",
+      "content": "## RegistrationThe workshop is organized in *hybrid* form. The workshop is primarily by invitation.However, we do have the possibility of accepting a small number of self-applications.  The **participation** can be- In presence, with the option of giving a talk,- Remotely, with the option of giving a talk,- For early-career researchers (Ph.D. Students) we are organizing a set of short10-minute communication to briefly introduces what they are working on and pitchtheir ideas.If this application is accepted, the organization will cover the[accommodation](/venue.html) costs.   {{site.data.alerts.warning}}The number of in presence partecipant is restricted to 30 people.The firm deadline for submitting your participation request is May, 13th.{{site.data.alerts.end}}{{site.data.alerts.important}}The abstract must be produced with the following  speaker-abstract.textemplate and sent by email to  fractional@gmx.com address having renamed it withthe surname of the speaker instead of the `speaker` placeholder. To check the appearance of the abstract, you can compile the speaker-abstract.texfile with:{% highlight bash %}pdflatex speaker-abstract.tex{% endhighlight %}{{site.data.alerts.end}}{{site.data.alerts.note}}For information write to fractional@gmx.com.{{site.data.alerts.end}}",
+      "url": "/registration.html",
+      "href": "/registration.html"
+    }
+    ,
+  
+    
+    "specialvolume-html": {
+      "title": "Special volume",
+      "category": "",
+      "content": "## Special volumeWe wish to **collect contributes** for a volume in the [ Springer INdAM Series](https://www.springer.com/series/10283).![indam](/img/indam.jpg){:style=\"float: right; padding-left: 10px;\"}This series publishes textbooks, multi-authors books, thesis and monographs in*English language* resulting from workshops, conferences, courses, schools,seminars, doctoral thesis, and research activities carried out with the supportof the [ INdAM - Istituto Nazionale di Alta Matematica](http://www.altamatematica.it/en).The books in the series usually discuss recent results and analyze new trends inmathematics and its applications.- The series is index in **Scopus** with a citescore of 0.9 in 2019 putting it in the 40th  percentile for the *general mathematics* section,- The $$\\LaTeX$$ template for contriuted books can be downloaded from [ here](https://resource-cms.springernature.com/springer-cms/rest/v1/content/22268/data/v1), please referto the [ Springer webpage](https://www.springer.com/gp/authors-editors/book-authors-editors/your-publication-journey/manuscript-preparation) for stylistic matter and informations.{% include note.html content=\"The submission procedures are TBD.\"%}",
+      "url": "/specialvolume.html",
+      "href": "/specialvolume.html"
+    }
+    ,
+  
+    
+    "venue-html": {
+      "title": "Venue",
+      "category": "",
+      "content": "## VenueThe workshop will take place at [ **Best Western Globus Hotel**](https://www.globushotel.com/) in Viale Ippocrate,119, 00161, Roma (RM), inside the **Ping room**.The **Best Western Globus Hotel** can be easily reached-  **By car** From any direction, follow the signs for the \"Grande Raccordo Anulare\"and take Via Tiburtina. Then follow the signs for either the University of Rome\"La Sapienza\" or the \"Policlinico\" Hospital. Just before the University, turnright into Via del Castro Laurenziano.-  **By plane**  - From Rome Fiumicino Leonardo da Vinci Airport, use the Trenitalia train  connection, a taxi or the Cotral bus service to reach Rome Termini Central  Station. From here see \"By Train\".  - From Rome Ciampino Giovan Battista Pastine Airport, reach Rome Termini Central  Station by taxi, by bus (service of the Atral, Cotral, Sit and Terravision  companies) or by train + bus (train to Ciampino and Cotral / Schiaffini bus).  From here see \"By Train\"-  **By train**  - From Roma Termini Station, take bus 310 or Metro line B in the direction of  Rebibbia to the Policlinico stop. Then take Viale Regina Margherita along the  Policlinico Umberto Primo and turn left into Viale Ippocrate.  - From Rome Tiburtina Station take bus 492-490-495.### AccommodationFor the invited participants the accomodation will be covered by theorganization at either the **Best Western Globus Hotel** (the conference venue)or the [ **Casa dell'Aviatore**](https://www.casaviatore.it) located in Viale dell'Università, 20 -- 00185, Rome.From the latter, the conference venue can then be easily reached on foot.  ",
+      "url": "/venue.html",
+      "href": "/venue.html"
+    }
+    ,
+  
+    
+      
+    
+      
+};
